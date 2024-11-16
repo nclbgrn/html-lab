a660d027c3868106b5d48108b7fa761e2b8e8752
nclbgrn <bugarinnicole9@gmail.com>
2024-11-16 00:52:04 -0700

    Add and style greetings

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// 1. Alert Button and Hover functionality
+// Alert Button and Hover functionality
 const alertButton = document.getElementById("btn-alert");
 alertButton.addEventListener("click", function() {
     alert("Hello world!");
@@ -9,3 +9,18 @@ alertButton.addEventListener("mouseover", function() {
 alertButton.addEventListener("mouseout", function() {
     alertButton.innerHTML = "Click me";
 });
+
+// Portfolio Greeting based on the time of day
+let greeting = document.createElement("h2");
+let currentHour = new Date().getHours();
+if (currentHour < 12) {
+    greeting.innerHTML = "Good morning!";
+    greeting.classList.add("morning");
+} else if (currentHour >= 12 && currentHour < 17) {
+    greeting.innerHTML = "Good afternoon!";
+    greeting.classList.add("afternoon");
+} else {
+    greeting.innerHTML = "Good evening!";
+    greeting.classList.add("evening");
+}
+document.body.insertBefore(greeting, document.body.firstChild);
